581453e40ec9560f18959edd905383997f6d998f
thepasswordispasswor <73312786+thepasswordispasswor@users.noreply.github.com>
2021-03-23 08:11:42 -0500

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,41 +1,44 @@
 var c = document.getElementById("game");
 var ctx = c.getContext("2d");
-ctx.fillStyle = "#383838";
-var s = 0.3;
-ctx.fillRect(0, 0, 150*s, 150*s);
-ctx.beginPath();
-ctx.moveTo(5*s,7*s);
-ctx.lineTo(5*s,143*s);
-ctx.lineTo(15*s,133*s);
-ctx.lineTo(15*s,17*s);
-ctx.closePath();
-ctx.fillStyle = "#505050";
-ctx.fill();
-
-ctx.beginPath();
-ctx.moveTo(7*s,5*s);
-ctx.lineTo(143*s,5*s);
-ctx.lineTo(133*s,15*s);
-ctx.lineTo(17*s,15*s);
-ctx.closePath();
-ctx.fill();
-
-ctx.beginPath();
-ctx.moveTo(145*s,7*s);
-ctx.lineTo(135*s,17*s);
-ctx.lineTo(135*s,133*s);
-ctx.lineTo(145*s,143*s);
-ctx.closePath();
-ctx.fillStyle = "#202020";
-ctx.fill();
-
-ctx.beginPath();
-ctx.moveTo(2.1,43.5);
-ctx.lineTo(5.1,40.5);
-ctx.lineTo(39.9,40.5);
-ctx.lineTo(42.9,43.5);
-ctx.closePath();
-ctx.fill();
-
-var towerTile = ctx.getImageData(0,0,150,150);
-ctx.putImageData(towerTile, 150, 150, 0, 0, 45, 45);
+var images = {
+};
+function loadImages(s) {
+	ctx.fillStyle = "#383838";
+	let scale = s;
+	ctx.fillRect(0, 0, 150*scale, 150*scale);
+	ctx.beginPath();
+	ctx.moveTo(5*scale,7*scale);
+	ctx.lineTo(5*scale,143*scale);
+	ctx.lineTo(15*scale,133*scale);
+	ctx.lineTo(15*scale,17*scale);
+	ctx.closePath();
+	ctx.fillStyle = "#505050";
+	ctx.fill();
+	ctx.beginPath();
+	ctx.moveTo(7*scale,5*scale);
+	ctx.lineTo(143*scale,5*scale);
+	ctx.lineTo(133*scale,15*scale);
+	ctx.lineTo(17*scale,15*scale);
+	ctx.closePath();
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.moveTo(145*scale,7*scale);
+	ctx.lineTo(135*scale,17*scale);
+	ctx.lineTo(135*scale,133*scale);
+	ctx.lineTo(145*scale,143*scale);
+	ctx.closePath();
+	ctx.fillStyle = "#202020";
+	ctx.fill();
+	
+	ctx.beginPath();
+	ctx.moveTo(7*scale,145*scale);
+	ctx.lineTo(17*scale,135*scale);
+	ctx.lineTo(133*scale,135*scale);
+	ctx.lineTo(143*scale,145*scale);
+	ctx.closePath();
+	ctx.fill();
+	
+	images.tower = ctx.getImageData(0,0,150*scale,150*scale);
+	ctx.putImageData(images.tower, 150, 150);
+}
