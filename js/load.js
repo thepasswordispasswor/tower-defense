258edc7b42621e08c2db43b2d6e01dc065c3856b
thepasswordispasswor <73312786+thepasswordispasswor@users.noreply.github.com>
2021-03-26 13:37:50 -0500

    Update load.js

--- js/load.js
+++ js/load.js
@@ -5,42 +5,42 @@ var images = {
 function loadImages(s) {
 	ctx.fillStyle = "#383838";
 	let scale = s;
-	ctx.fillRect(0, 0, 150*scale, 150*scale);
+	ctx.fillRect(0, 0, 30*scale, 30*scale);
 	ctx.beginPath();
-	ctx.moveTo(5*scale,7*scale);
-	ctx.lineTo(5*scale,143*scale);
-	ctx.lineTo(15*scale,133*scale);
-	ctx.lineTo(15*scale,17*scale);
+	ctx.moveTo(1*scale,1*scale);
+	ctx.lineTo(1*scale,29*scale);
+	ctx.lineTo(3*scale,27*scale);
+	ctx.lineTo(3*scale,3*scale);
 	ctx.closePath();
 	ctx.fillStyle = "#505050";
 	ctx.fill();
 	ctx.beginPath();
-	ctx.moveTo(7*scale,5*scale);
-	ctx.lineTo(143*scale,5*scale);
-	ctx.lineTo(133*scale,15*scale);
-	ctx.lineTo(17*scale,15*scale);
+	ctx.moveTo(1*scale,1*scale);
+	ctx.lineTo(29*scale,1*scale);
+	ctx.lineTo(27*scale,3*scale);
+	ctx.lineTo(3*scale,3*scale);
 	ctx.closePath();
 	ctx.fill();
 	
 	ctx.beginPath();
-	ctx.moveTo(145*scale,7*scale);
-	ctx.lineTo(135*scale,17*scale);
-	ctx.lineTo(135*scale,133*scale);
-	ctx.lineTo(145*scale,143*scale);
+	ctx.moveTo(29*scale,1*scale);
+	ctx.lineTo(27*scale,3*scale);
+	ctx.lineTo(27*scale,27*scale);
+	ctx.lineTo(29*scale,29*scale);
 	ctx.closePath();
 	ctx.fillStyle = "#202020";
 	ctx.fill();
 	
 	ctx.beginPath();
-	ctx.moveTo(7*scale,145*scale);
-	ctx.lineTo(17*scale,135*scale);
-	ctx.lineTo(133*scale,135*scale);
-	ctx.lineTo(143*scale,145*scale);
+	ctx.moveTo(1*scale,29*scale);
+	ctx.lineTo(3*scale,27*scale);
+	ctx.lineTo(27*scale,27*scale);
+	ctx.lineTo(29*scale,29*scale);
 	ctx.closePath();
 	ctx.fill();
 	
-	images["baseplate"] = ctx.getImageData(0,0,150*scale,150*scale);
+	images["baseplate"] = ctx.getImageData(0,0,30*scale,30*scale);
 	ctx.clearRect(0,0,300,300);
 }
-loadImages(0.3);
+loadImages(1);
 ctx.putImageData(images["baseplate"], 150, 150);
