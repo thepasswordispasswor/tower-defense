47c67559354f051a6ce60d5bcbfe012ddc1c8394
thepasswordispasswor <73312786+thepasswordispasswor@users.noreply.github.com>
2021-03-26 16:43:30 -0500

    Update load.js

--- js/load.js
+++ js/load.js
@@ -2,6 +2,11 @@ var c = document.getElementById("game");
 var ctx = c.getContext("2d");
 var images = {
 };
+
+var p = {
+	map: {},
+}
+
 function loadImages(s) {
 	ctx.fillStyle = "#383838";
 	let scale = s;
@@ -32,4 +37,43 @@ function loadImages(s) {
 	ctx.clearRect(0,0,300,300);
 }
 loadImages(1);
-ctx.putImageData(images["baseplate"], 150, 150);
+
+function loadMap(map) {
+	let temp = Object.assign({}, p.map);
+	p.map = map;
+	return temp;
+}
+
+function displayMap() {
+	let m = p.map;
+	for(let i = 0; i < m.height; i++) {
+		for(let o = 0; o < m.width; o++) {
+			ctx.putImageData(m[i+""+o], o*30, i*30);
+		}
+	}
+}
+
+function Map(width, height, mapInfo) {
+	this.width = width;
+	this.height = height;
+	for(let i = 0; i < height; i++) {
+		for(let o = 0; o < width; o++) {
+			this[i+""+o] = mapInfo[i][o];
+		}
+	}
+}
+
+var snakeMap = new Map(9, 9, [
+	["basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate",	],
+	["start",	"", 		"", 		"", 		"", 		"", 		"", 		"", 		"basePlate",	],
+	["basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"", 		"basePlate",	],
+	["basePlate", 	"", 		"",		"",		"",		"",		"",		"",		"basePlate",	],
+	["basePlate", 	"",		"basePlate",	"basePlate",	"basePlate",	"basePlate",	"basePlate",	"basePlate",	"basePlate",	],
+	["basePlate", 	"",		"",		"",		"",		"",		"",		"",		"basePlate",	],
+	["basePlate", 	"basePlate",	"basePlate",	"basePlate",	"basePlate",	"basePlate",	"basePlate",	"", 		"basePlate",	],
+	["basePlate", 	"", 		"", 		"", 		"", 		"", 		"". 		"",		"end",		],
+	["basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate", 	"basePlate",	],
+]);
+
+loadMap(snakeMap);
+displayMap();
