18044bbeef2b7364fda6b0659f8faa2970acc11f
thepasswordispasswor <73312786+thepasswordispasswor@users.noreply.github.com>
2021-04-07 12:50:38 -0500

    Update load.js

--- js/load.js
+++ js/load.js
@@ -529,3 +529,12 @@ function test() {
 	loadMap(snakeMap);
 	displayMap();
 }
+
+function openCity(tabName) {
+ 	let i;
+ 	let x = document.getElementsByClassName("tab");
+ 	for (i = 0; i < x.length; i++) {
+ 		x[i].style.display = "none";  
+ 	}
+ 	document.getElementById(tabName).style.display = "block";  
+}
