819fc03f36fc3289b1835924499db42200aba310
thepasswordispasswor <73312786+thepasswordispasswor@users.noreply.github.com>
2021-04-07 13:14:23 -0500

    Update load.js

--- js/load.js
+++ js/load.js
@@ -536,5 +536,5 @@ function openTab(tabName) {
  	for (i = 0; i < x.length; i++) {
  		x[i].style.display = "none";  
  	}
- 	document.getElementById(tabName).style.display = "block";  
+ 	document.getElementById(tabName).style.display = "inline-block";  
 }
